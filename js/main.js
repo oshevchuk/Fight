618989a832e18fb627b80afd460d0390598f06d7
megadevice83 <megadevice83@gmail.com>
2017-06-14 08:44:46 +0300

    + images
    //start v2 absolute position - center fix

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Oshevchuk on 13.06.2017.
+ */
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.preloader').delay(2000).queue(function () {
+        $('.preloader').hide();
+        // $('.preloader').fadeIn(300);
+
+        $('.mask').show();
+        $('.home').show();
+    });
+});
